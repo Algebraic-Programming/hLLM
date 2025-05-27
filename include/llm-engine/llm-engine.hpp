069bbb4910b524609b0d59208f90b411f5ebd013
Sergio Martin <sergio.miguel.martin@huawei.com>
2025-05-27 14:40:08 +0200

    Adding HiCR as dependency

--- include/llm-engine/llm-engine.hpp
+++ include/llm-engine/llm-engine.hpp
@@ -4,6 +4,10 @@
 #include <hicr/core/exceptions.hpp>
 #include <hicr/core/definitions.hpp>
 #include <deployr/deployr.hpp>
+#include <taskr/taskr.hpp>
+#include <hicr/backends/hwloc/topologyManager.hpp>
+#include <hicr/backends/boost/computeManager.hpp>
+#include <hicr/backends/pthreads/computeManager.hpp>
 
 namespace llmEngine
 {
@@ -14,6 +18,43 @@ class LLMEngine final
 
   LLMEngine()
   {
+    // initializing hwloc topology object
+    hwloc_topology_init(&_topology);
+
+    // Initializing HWLoc-based host (CPU) topology manager
+    HiCR::backend::hwloc::TopologyManager tm(&_topology);
+
+    // Asking backend to check the available devices
+    const auto t = tm.queryTopology();
+
+    // Compute resources to use
+    HiCR::Device::computeResourceList_t computeResources;
+
+    // Getting compute resources in this device
+    auto cr = (*(t.getDevices().begin()))->getComputeResourceList();
+
+    // Adding it to the list
+    auto itr = cr.begin();
+    for (int i = 0; i < 1; i++)
+    {
+      computeResources.push_back(*itr);
+      itr++;
+    }
+
+    // Initializing Boost-based compute manager to instantiate suspendable coroutines
+    HiCR::backend::boost::ComputeManager boostComputeManager;
+
+    // Initializing Pthreads-based compute manager to instantiate processing units
+    HiCR::backend::pthreads::ComputeManager pthreadsComputeManager;
+
+    // Creating taskr
+    _taskr = std::make_unique<taskr::Runtime>(&boostComputeManager, &pthreadsComputeManager, computeResources);
+  }
+
+  ~LLMEngine()
+  {
+    // Freeing up memory
+    hwloc_topology_destroy(_topology);
   }
 
   __INLINE__ void initialize(int *pargc, char ***pargv)
@@ -21,14 +62,21 @@ class LLMEngine final
     // Initializing DeployR
     _deployr.initialize(pargc, pargv);
 
-  }
+    // Initializing computing resources for TaskR deployment
+    
 
-  ~LLMEngine() = default;
+  }
 
   private:
 
   // DeployR instance
   deployr::DeployR _deployr;
+
+  // TaskR instance
+  std::unique_ptr<taskr::Runtime> _taskr;
+
+  // Creating HWloc topology object
+  hwloc_topology_t _topology;
 
 }; // class LLMEngine
 
